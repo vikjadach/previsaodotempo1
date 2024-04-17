2b50fe54b61031a456a20c6bc4dfa2bb7be4dc16
previsaodotempo1 <victoriaferjc@gmail.com>
2024-04-17 11:52:03 -0300

    001

---
+++ previsaodotempo1/Results.cs
@@ -0,0 +1,13 @@
+público class results
+
+públic int temp {get,set;}
+públic string description {get,set;}
+públic string city {get,set;}
+públic Double rain {get,set;}
+públic int humidity {get,set;}
+públic string sunrise {get,set;}
+públic string sunset {get,set;}
+públic string wind_speedy{get,set;}
+públic string wind_direction{get,set;}
+públic string moon_phase {get,set;}
+públic string currently {get,set;}
