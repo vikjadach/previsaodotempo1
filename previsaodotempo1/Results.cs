588c6f6039fceb889e46c28129286a8cdba743e4
previsaodotempo1 <victoriaferjc@gmail.com>
2024-05-02 08:58:27 -0300

    01

--- previsaodotempo1/Results.cs
+++ previsaodotempo1/Results.cs
@@ -1,13 +1,21 @@
-público class results
+namespace PREVISAODOTEMPO1;
 
-públic int temp {get,set;}
-públic string description {get,set;}
-públic string city {get,set;}
-públic Double rain {get,set;}
-públic int humidity {get,set;}
-públic string sunrise {get,set;}
-públic string sunset {get,set;}
-públic string wind_speedy{get,set;}
-públic string wind_direction{get,set;}
-públic string moon_phase {get,set;}
-públic string currently {get,set;}
+public class results 
+{
+	public int temp { get;set;}
+	public string description{get;set;}
+	public string city{get;set;}
+	public double rain{get;set;}
+	public int humidity{get;set;}
+	public string sunrise{get;set;}
+	public string sunset{get;set;}
+	public string wind_speedy{get;set;}
+	public int wind_direction{get;set;}
+	public string wind_cardinal{get;set;}
+	public string moon_phase{get;set;}
+	public string currently{get;set;}
+	public string codition_code{get;set;}
+	public string img_id{get;set;}
+	public string claudiness{get;set;}
+
+}
---
+++ previsaodotempo1/resposta.cs
@@ -0,0 +1,6 @@
+using PREVISAODOTEMPO1;
+
+public class Resposta
+{
+    public Results results{get;set;}
+}
